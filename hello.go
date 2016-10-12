2ca9f5d0be6ad5cfb325c0a0b3aa756c8c284049
Eduard Coll <ecollvila@gmail.com>
2016-10-12 15:16:15 +0200

    Importing my first library in go

--- hello.go
+++ hello.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/hocklo/stringutil"
+)
 
 func main() {
 	fmt.Printf("hello, world\n")
+	// System output using the library stringutil
+	fmt.Printf(stringutil.Reverse("!oG ,olleH")+"\n")
 }
 
